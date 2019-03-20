1eac2a2da1e9263c6beda70aa774da52903f1bff
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-03-20 21:01:49 +0100

    save and undo icons added

--- Constants/ImageConstants.cs
+++ Constants/ImageConstants.cs
@@ -6,5 +6,6 @@ namespace HandsOnLab1.Constants
         /// The Save Icon
         /// </summary>
         public static readonly string SAVE_ICON = @"pack://application:,,,/HandsOnLab1;component/Images/Save.gif";
+        public static readonly string UNDO_ICON = @"pack://application:,,,/HandsOnLab1;component/Images/Undo.gif";
     }
 }
