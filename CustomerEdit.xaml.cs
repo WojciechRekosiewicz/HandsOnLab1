3c33443b01cbf9ab3f2c11428a242c3e228bbdff
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-03-20 21:27:48 +0100

    Added columns in Customer Edit

--- CustomerEdit.xaml.cs
+++ CustomerEdit.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HandsOnLab1.ClientEntities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,14 +18,30 @@ namespace HandsOnLab1
     /// <summary>
     /// Interaction logic for CustomerEdit.xaml
     /// </summary>
-    public partial class CustomerEdit : UserControl
+    public partial class CustomerEdit : UserControl, INotifyPropertyChanged
     {
+        private CustomerUpdate _customer = new CustomerUpdate(123);
+
         public CustomerEdit()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
-        private void CanSave(object sender, CanExecuteRoutedEventArgs e)
+        public CustomerUpdate Customer
+        {
+            get { return _customer; }
+            set
+            {
+                if(_customer != value)
+                {
+                    _customer = value;
+                    OnPropertyChanged("Customer");
+                }
+            }
+
+        }
+    private void CanSave(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
         }
@@ -32,5 +50,27 @@ namespace HandsOnLab1
         {
             MessageBox.Show("Assume the save worked");
         }
+
+                #region INotifyPropertyChanged Members
+                /// <summary>
+                /// Implicit implementation of the
+                //INotifyPropertyChanged.PropertyChanged event.
+                /// </summary>
+        public event PropertyChangedEventHandler
+                PropertyChanged;
+                /// <summary>
+                /// Throws the <c>PropertyChanged</c> event.
+                /// </summary>
+                /// <param name="propertyName">The name of the
+                // property that was modified.</param>
+        protected void OnPropertyChanged(string propertyName)
+                {
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new
+                        PropertyChangedEventArgs(propertyName));
+                    }
+                }
+            #endregion
     }
 }
